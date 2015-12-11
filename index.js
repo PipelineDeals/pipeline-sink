4a6e24bce33a453a29582cc4654ae51274bb716d
Brandon Hilkert <brandonhilkert@gmail.com>
2015-12-11 09:49:39 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,38 @@
+var redisUrl = process.env.MESSAGE_BUS_REDIS_URL || "127.0.0.1";
+var redis = require("redis"),
+    redisClient = redis.createClient(6379, redisUrl);
+var AWS = require('aws-sdk');
+
+AWS.config.region = 'us-east-1';
+var kinesis = new AWS.Kinesis();
+
+redisClient.on("ready", function () {
+  console.log("Connected to Redis!");
+  redisClient.psubscribe("*");
+});
+
+redisClient.on("pmessage", function (pattern, channel, message) {
+  // var c = channel.split(":");
+  // var model = c[0];
+  // var action = c[1];
+  // console.log("Model: ", model);
+  // console.log("Action: ", action);
+  // console.log("Message: ", message);
+
+  var params = {
+    Data: message, /* required */
+    PartitionKey: channel, /* required */
+    StreamName: 'mantle', /* required */
+  };
+
+  kinesis.putRecord(params, function(err, data) {
+    if (err) console.log(err, err.stack); // an error occurred
+    // else     console.log(data);           // successful response
+  });
+});
+
+redisClient.on("error", function (err) {
+  console.log("Redis error: " + err);
+});
+
+
